ef5bf700b625d4fe903fdac8e91fafa15ea6d22b
ROHIT YADAV <163042643+DerHarteCoder@users.noreply.github.com>
2025-07-23 23:51:31 +0530

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,7 +7,7 @@ function Experience() {
         <h2 className="text-3xl font-bold text-accent-color mb-8 text-center">Where I've Worked</h2>
         <div className="mb-8 card"> {/* Applied .card class here */}
           <h3 className="card-heading">Frontend Developer</h3> {/* Applied .card-heading class here */}
-          <p className="text-dark-text-secondary mb-2">JESM | 01/2022 - Present</p>
+          <p className="text-dark-text-secondary mb-2">Bootcamp pvt ltd | 01/2022 - Present</p>
           <ul className="list-disc list-inside card-description"> {/* Applied .card-description class here */}
             <li>Engineered complex single-page applications using React, achieving a 20% increase in performance and a 20% improvement in responsiveness.</li>
             <li>Integrated SonarQube into the CI/CD pipeline, automating code quality checks and ensuring adherence to coding standards. Executed project development using Agile SCRUM methodology, ensuring iterative delivery, continuous integration, and rapid response to changing requirements.</li>
